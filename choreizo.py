7e04a8c6ed6da85042c222b1497f4b0edafdc379
Eyal Shahar <eshahar@exploratorium.edu>
2021-08-18 23:32:31 -0700

    weekly chores

--- choreizo.py
+++ choreizo.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 import os.path
 import json
 import time
+import datetime
 
 chore_file = "sausage.json"
 
@@ -20,34 +21,67 @@ frequencies = {
   "bimonthly": 60,
 }
 
+day_names = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
 minutes_in_day = 60 * 60 * 24
+
+def must_do(chore):
+  if not 'weekday' in chore:
+    return False
+  today = datetime.datetime.today()
+  
+  # not right day of the week?
+  if not today.weekday() == day_names.index(chore['weekday']):
+    return False
+  
+  # already checked today?
+  if abs(today - datetime.datetime.fromtimestamp(chore['last_completed'])) < datetime.timedelta(days=1):
+    return False
+  
+  # weeks in month specified?
+  if 'weeks' in chore:
+    week_in_month = int(today.day / 7)
+    # is this the correct week?
+    if week_in_month in chore['weeks']:
+      return True
+  # do this chore every week
+  else:
+    return True
 
 def should_do(chore):
   # Calculates the amount of time since the last completion time
   # and returns whether or not it goes on the todo list.
+  if not 'frequency' in chore:
+    return False
   days_since_completion = (int(time.time()) - chore['last_completed']) / minutes_in_day
   return days_since_completion - frequencies[chore['frequency']] > 0
-
+  
 def get_chores():
   with open(chore_file) as f:
     chore_list = json.load(f)
 
   # Calculate the todo and done lists
+  mustdo = []
   todo = []
   done = []
   for chore in chore_list:
-    if should_do(chore):
-      todo.append(chore)
-    else:
-      try:
-        chore['next_due_date']
-      except KeyError:
-        chore['next_due_date'] = int(time.time()) + frequencies[chore['frequency']] * minutes_in_day
-      done.append(chore)
+    if must_do(chore):
+      mustdo.append(chore)
+    elif 'frequency' in chore:
+      if should_do(chore):
+        todo.append(chore)
+      else:
+        try:
+          chore['next_due_date']
+        except KeyError:
+          chore['next_due_date'] = int(time.time()) + frequencies[chore['frequency']] * minutes_in_day
+        done.append(chore)
 
   # Sort the chores by priority
-  return {'todo': sorted(todo, key=lambda chore: frequencies[chore['frequency']], reverse=True),
-          'done': sorted(done, key=lambda chore: frequencies[chore['frequency']], reverse=True)}
+  return {
+    'mustdo': mustdo,
+    'todo': sorted(todo, key=lambda chore: frequencies[chore['frequency']], reverse=True),
+    'done': sorted(done, key=lambda chore: frequencies[chore['frequency']], reverse=True)}
 
 def dismiss_chore(dismissed_chore):
   with open(chore_file) as f:
@@ -57,7 +91,8 @@ def dismiss_chore(dismissed_chore):
       continue
     now = int(time.time())
     chore['last_completed'] = now
-    chore['next_due_date'] = now + frequencies[chore['frequency']] * minutes_in_day
+    if 'frequency' in chore:
+      chore['next_due_date'] = now + frequencies[chore['frequency']] * minutes_in_day
   with open(chore_file, 'w') as f:
     json.dump(chore_list, f, ensure_ascii=True, indent=2, sort_keys=True)
 
@@ -83,6 +118,7 @@ def hello(name=None):
   chores = get_chores()
   return render_template(
     'index.html', 
+    mustdo_chores=chores['mustdo'],
     todo_chores=chores['todo'], 
     done_chores=chores['done'])
 
